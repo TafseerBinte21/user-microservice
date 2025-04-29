451ea0724e378fa2326c25aa1219d5a05711e5d8
Rimpi <tafseerrimpi@gmail.com>
2025-04-30 04:12:12 +0600

    first commit

---
+++ api-gateaway/src/test/java/com/tafa/lib/ApiGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tafa.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
